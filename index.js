b7b3f0b58b29b0430a42d4f0012cb00cb791fe56
Kelechi David <kelechichiemela20@gmail.com>
2023-09-07 16:54:37 +0100

    2nd commit

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const moment = require('moment');
 
-
 const app = express();
-
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 
 app.get('/api', (req, res) => {
   const { slack_name } = req.query;
-  const track = "backend"; 
+  const track = "backend";
   const currentDay = moment().format('dddd');
   const currentUTCTime = moment().utc().format();
 
@@ -17,8 +15,8 @@ app.get('/api', (req, res) => {
     current_day: currentDay,
     utc_time: currentUTCTime,
     track,
-    github_file_url: 'https://github.com/kelechidavid/stage-one-HNG-nodejs.git/index.js',   
-    github_repo_url: 'https://github.com/kelechidavid/stage-one-HNG-nodejs.git',
+    github_file_url: 'https://github.com/kelechidavid/stage-one-HNG-nodejs/blob/8ca461b535dfe67e3cb1ad341b67ae06f5d2bd2c/index.js', // Corrected GitHub file URL
+    github_repo_url: 'https://github.com/kelechidavid/stage-one-HNG-nodejs.git', // Corrected GitHub repository URL
     status_code: 200,
   };
 
